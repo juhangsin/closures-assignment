12b304fdd17ea9bd34991b518dd066a3ab2bb982
Ju-Hang Sin <juhangsin@gmail.com>
2017-07-09 17:28:38 -0400

    ex 5 completed

--- closure_exercises/exercise5.js
+++ closure_exercises/exercise5.js
@@ -2,9 +2,13 @@
 // Generate another function that requires arguments.
 // This time you'll have to set up the flyTo variable yourself.
 
-var personToVisit = "best friend";
+
 // YOUR CODE HERE ------------------------
 
+function flyTo(city) {
+	var personToVisit = "best friend";
+	console.log("Enjoy your visit to " + city + " to visit your " + personToVisit);
+}
 
 
 // ---------------------------------------
